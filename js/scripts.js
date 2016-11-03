023f5e64d2c1c791babb91a37d7b74592fffcd34
Austin Blain and Erica Nafziger <austin@>
2016-11-03 13:45:10 -0700

    tell/show the user whose turn it is

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,8 @@ var Player = function(name, symbol, winCount) {
   this.winCount = winCount;
 }
 
-var playerOne = new Player("playerOne", "X", 0);
-var playerTwo = new Player("playerTwo", "O", 0);
+var playerOne = new Player("Player One", "X", 0);
+var playerTwo = new Player("Player Two", "O", 0);
 
 var currentPlayer = playerOne;
 
@@ -23,7 +23,12 @@ function switchPlayer() {
 }
 function checkWinner() {
   if (allValues[0] === currentPlayer.symbol && allValues[1] === currentPlayer.symbol && allValues[2] === currentPlayer.symbol || allValues[3] === currentPlayer.symbol && allValues[4] === currentPlayer.symbol && allValues[5] === currentPlayer.symbol || allValues[6] === currentPlayer.symbol && allValues[7] === currentPlayer.symbol && allValues[8] === currentPlayer.symbol || allValues[0] === currentPlayer.symbol && allValues[4] === currentPlayer.symbol && allValues[8] === currentPlayer.symbol || allValues[0] === currentPlayer.symbol && allValues[3] === currentPlayer.symbol && allValues[6] === currentPlayer.symbol || allValues[1] === currentPlayer.symbol && allValues[4] === currentPlayer.symbol && allValues[7] === currentPlayer.symbol || allValues[2] === currentPlayer.symbol && allValues[5] === currentPlayer.symbol && allValues[8] === currentPlayer.symbol || allValues[2] === currentPlayer.symbol && allValues[4] === currentPlayer.symbol && allValues[6] === currentPlayer.symbol) {
-    alert(currentPlayer.names + " wins!");
+    $("#winnerIs").text(currentPlayer.names + " wins!");
+    allValues = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    $(".btn").text("");
+    // $(".col-xs-4 .btn").attach("click");
+  } else if (allValues[0] !== 0 && allValues[1] !== 0 && allValues[2] !== 0 && allValues[3] !== 0 && allValues[4] !== 0 && allValues[5] !== 0 && allValues[6] !== 0 && allValues[7] !== 0 && allValues[8] !== 0) {
+    $("#winnerIs").text("You're both losers!");
     allValues = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     $(".btn").text("");
   }
@@ -34,13 +39,25 @@ function checkWinner() {
 
 $(document).ready(function() {
   debugger;
+  $("#playerOneName").text(playerOne.names)
+  $("#playerTwoName").text(playerTwo.names)
+  $("#playerOneName").addClass("chosen");
 
   $(".col-xs-4 .btn").click(function() {
     debugger;
+    if (currentPlayer.names === playerOne.names) {
+      $("#playerOneName").removeClass("chosen");
+      $("#playerTwoName").addClass("chosen");
+    } else {
+      $("#playerTwoName").removeClass("chosen");
+      $("#playerOneName").addClass("chosen");
+    }
+    $("#winnerIs").text("");
     var buttonValue = parseInt($(this).val());
     $(this).text(currentPlayer.symbol);
     allValues[buttonValue] = currentPlayer.symbol;
     checkWinner();
+    // $(this).off("click");
     switchPlayer();
     console.log(allValues);
   });
